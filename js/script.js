03210b0b544022bff1dabc07f53608fa05e680db
AlinaMurMur <alinamurtazina777@yandex.ru>
2023-12-21 16:15:13 +0300

    changes drop-down

--- js/script.js
+++ js/script.js
@@ -1,26 +1,33 @@
 let elems = document.querySelectorAll('.navigation-drop-down');
+let button = document.querySelectorAll(".navigation-button");
 
-document.querySelectorAll(".navigation-button").forEach(button => {
+button.forEach(button => {
   button.addEventListener('click', function(event) {
     elems.forEach(link => {
-      if (link.querySelector('.navigation-button') !== button) {
+      if (link.button !== button) {
         link.firstElementChild.classList.remove('navigation-link-open')
       }
     })
 
-    button.classList.toggle('navigation-link-open')
+    button.classList.toggle('navigation-link-open');
 
     event.preventDefault();
+    event.stopPropagation();
   })
-})
+});
 
-document.body.addEventListener('click', function(event) {
-  if (
+document.addEventListener('click', function(event) {
+  let isClickInside = event.target.classList.contains('navigation-link-active');
+  if (isClickInside) {
     event.target.classList.contains('navigation-button') == true ||
-    event.target.classList.contains('popover') == true
-    ) return
-
+    event.target.classList.contains('popover') == true ||
+    event.target.classList.contains('navigation-text')
+  } else {
     elems.forEach(link => {
-    link.firstElementChild.classList.remove('navigation-link-open')
-  })
-})
+        link.firstElementChild.classList.remove('navigation-link-open')
+      })
+  }
+});
+
+
+/// slider
